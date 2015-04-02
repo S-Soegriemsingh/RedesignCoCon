9fef44305b15214ac612831a76d87c89680d7581
Sheik Soegriemsingh <s.soegriemsingh@gmail.com>
2015-04-03 01:19:20 +0200

    Changes to Jolo

--- CoCon/page-jolo.php
+++ CoCon/page-jolo.php
@@ -3,33 +3,27 @@
 		<div class='main_container'>
 			<header>
 					<h1>
-						<a href='index.php'>Coloured Consultancy</a>
+						<a href='index.php'>Jongeren lossen het op</a>
 					</h1>
 			</header>
 			<div class='content'>
-				<div class='box buurtbemiddeling'>
-					<a href='http://www.buurtbemiddelingnederland.nl'>
-						<h2>Buurtbemiddeling</h2>
-						<p>Voor en door bewoners</p>
-					</a>
+				<div class='box info'>
+					<h2>Content</h2>
+					<p>stuff</p>
 				</div>
-				<div class='box jolo'>
-					<a href='jolo'>
-						<h2>JOLO</h2>
-						<p>Jongeren lossen het op</p>
-					</a>
+				<div class='box image'>
+					<h2>Image</h2>
+					<p>stuff</p>
 				</div>
-				<div class='box mediation'>
-					<a href='http://www.coconmediation.nl/'>
-						<h2>Mediation</h2>
-						<p>Klacht is kracht</p>
-					</a>
+				<div class='box testimonials'>
+					<h2>Testimonials</h2>
+					<p>stuff</p>
 				</div>
 			</div>
 			<nav>				
 				<ul>
-					<li><a href='over-cocon'>Over Cocon</a></li>
-					<li><a href='nieuws'>Nieuws</a></li>
+					<li><a href='over-jolo'>Over Jolo</a></li>
+					<li><a href='jolo-nieuws'>Nieuws</a></li>
 					<li><a href='contact'>Contact</a></li>
 				</ul>				
 			</nav>
