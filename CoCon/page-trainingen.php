fb2898835038ed9e242c2bc054ffc704404bd20b
Cas van 't Wout <Cas@Cass-MacBook-Pro.local>
2015-04-03 02:01:01 +0200

    Created all pages with their own colors and names (CSS and PHP)

---
+++ CoCon/page-trainingen.php
@@ -0,0 +1,30 @@
+<?php get_header(); ?>
+<body>
+		<div class='main_container'>
+			<header>
+					<h1>
+						<a href='index.php'>Trainingen</a>
+					</h1>
+			</header>
+			<div class='content'>
+				<div class='box info'>
+					<h2>Content</h2>
+					<p>stuff</p>
+				</div>
+				<div class='box image'>
+					<h2>Image</h2>
+					<p>stuff</p>
+				</div>
+				<div class='box testimonials'>
+					<h2>Testimonials</h2>
+					<p>stuff</p>
+				</div>
+			</div>
+			<nav class='bottomnav'>				
+				<ul>
+					<li><a href='over-cocon'>Over CoCon</a></li>
+					<li><a href='nieuws'>Nieuws</a></li>
+					<li><a href='contact'>Contact</a></li>
+				</ul>				
+			</nav>
+<?php get_footer(); ?>
