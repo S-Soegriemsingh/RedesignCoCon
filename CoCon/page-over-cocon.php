a83dd2f5c565e8880a43a278ab3365eeb107ade3
Cas van 't Wout <Cas@Cass-MacBook-Pro.local>
2015-04-06 17:06:28 +0200

    Changed header links

---
+++ CoCon/page-over-cocon.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+<body>
+		<div class='main_container'>
+			<header>
+					<h1>
+						<a href='index.php'>Over CoCon</a>
+					</h1>
+			</header>
+			<div class='content'>
+				<div class='box info'>
+					<a class='main' href='conflict.php'>
+						<h2>Van conflict naar contact</h2>
+					</a>
+				</div>
+				<div class='box image'>
+					<a class='main' href='diversiteit.php'>
+						<h2>De kracht van diversiteit</h2>
+					</a>
+				</div>
+				<div class='box testimonials'>
+					<a class='main' href='samen-leren.php'>
+						<h2>Samen leren samen delen.</h2>
+					</a>
+				</div>
+			</div>
+			<nav>				
+				<ul>
+					<li><a href='over-cocon'>Over CoCon</a></li>
+					<li><a href='nieuws'>Nieuws</a></li>
+					<li><a href='contact'>Contact</a></li>
+				</ul>				
+			</nav>
+<?php get_footer(); ?>
